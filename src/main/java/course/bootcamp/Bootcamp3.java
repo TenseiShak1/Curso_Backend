89225138f07f296a629ba14190588ac38f4510b0
luis <luis@luis-HP-Laptop-15-db0xxx>
2022-11-30 20:44:31 -0500

    Correción del Bootcamp3 e implementación del Bootcamp4

--- src/main/java/course/bootcamp/Bootcamp3.java
+++ src/main/java/course/bootcamp/Bootcamp3.java
@@ -8,25 +8,25 @@ public class Bootcamp3 {
         Persona persona =new Persona();
         Scanner sc = new Scanner (System.in);
         //Variables de control.
-        String newUser_name,newUser_lastname;
-        int numberPhone;
+        String newUser_name;
+        int numberPhone,edad;
         //Igreso de los datos.
         System.out.print("Ingrese algun nombre para este usuario:\n==>");
         newUser_name = sc.nextLine(); 
-        System.out.print("Ingrese algun apellido para este usuario:\n==>");
-        newUser_lastname = sc.nextLine();
+        System.out.print("Ingrese alguna  edad para este usuario:\n==>");
+        edad = sc.nextInt();
         System.out.print("Ingrese algun numero de telefono para este usuario:\n==>");
         numberPhone = sc.nextInt();
         //Aplicación de la clase persona
         //---------------------------------------------
         persona.setNombre(newUser_name);
-        System.out.println(persona.getNombre());
+        System.out.println("Nombre de la persona: "+persona.getNombre());
         
-        persona.setApellido(newUser_lastname);
-        System.out.println(persona.getApellido());
+        persona.setEdad(edad);
+        System.out.println("edad "+ persona.getEdad()+" años");
         
         persona.setTelefono(numberPhone);
-        System.out.println(persona.getTelefono());
+        System.out.println("Este es su numero de telefono: "+persona.getTelefono());
         //---------------------------------------------
     }
 }
@@ -36,7 +36,7 @@ class Persona {
     Metodos Privados
     */
     private String nombre;
-    private String apellido;
+    private int edad;
     private int telefono;
     //-----------------
     
@@ -51,11 +51,11 @@ class Persona {
         return nombre;
     }
     //Funciones Get y Set de la propiedad apellido
-    public void setApellido(String apellido){
-        this.apellido=apellido;
+    public void setEdad(int apellido){
+        this.edad=apellido;
     }
-    public String getApellido(){
-        return apellido;
+    public int getEdad(){
+        return edad;
     }
     //Funciones Get y Set de la propiedad telefono
     public void setTelefono(int telefono){
